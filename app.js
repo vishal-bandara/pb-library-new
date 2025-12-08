ad7decd02af78b30bc0c292649efb0240cc6cfd3
vishal-bandara <nethminavishalbandara@gmail.com>
2025-12-08 15:04:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/sw.js')
+        navigator.serviceWorker.register('sw.js')
             .then(reg => {
                 console.log('Service Worker registered successfully!', reg.scope);
             })
